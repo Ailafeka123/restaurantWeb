7f7f419f1083a84ad0224ae7c464ae5fe364b587
Ailafeka <ailafeka@gmail.com>
2025-04-23 18:05:46 +0800

    構思中 先嘗試排版

---
+++ src/components/HeaderComponents/HeaderNavbar.vue
@@ -0,0 +1,14 @@
+<script setup>
+    console.log("Header is loading");
+</script>
+<template>
+    <nav >
+        <ul class="headerNav">
+            <li>關於我們</li>
+            <li>最新消息</li>
+            <li>餐點</li>
+            <li>位置</li>
+            <li>預約</li>
+        </ul>
+    </nav>
+</template>
---
+++ src/components/MainComponent/Preview.vue
@@ -0,0 +1,33 @@
+<script setup>
+    import preview1 from "@/assets/preview1.jpg"
+</script>
+<template>
+    <div class="previewDiv" :style="{backgroundImage:` linear-gradient(to right, rgba(132, 25, 34,0), rgba(132, 25, 34,0.7)),url(${preview1})`}">
+        <!-- <img :src="preview1" alt=""> -->
+        <!-- linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(0,0,0,0.1)), -->
+        <!-- <div class="previewImg1" ></div> -->
+    </div>
+</template>
+<style>
+.previewDiv{
+    margin-top: var(--header-height);
+    height: max(500px,100vh);
+    background-position:top;
+    background-size: 100%;
+    background-repeat: no-repeat;
+    /* display: flex;
+    align-items: start;
+    justify-content: start; */
+    overflow: hidden;
+    /* background-color: #834f4f; */
+}
+
+
+.previewDiv img{
+    height: 100%;   
+    width: 30%;
+    overflow: hidden;
+    object-fit:cover;
+    object-position: center;
+}
+</style>
