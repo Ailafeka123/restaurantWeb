d393314da40071453b1d6c5ab5830b0d9f6095d9
Ailafeka <ailafeka@gmail.com>
2025-04-28 12:16:55 +0800

    測試v-lazy

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import './assets/global.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueLazyload from 'vue-lazyload'
+const app = createApp(App);
+app.use(VueLazyload,{
+    loading:"@/assets/preview3.jpg",
+    error:''
+})
 
-createApp(App).mount('#app')
+app.mount('#app')
