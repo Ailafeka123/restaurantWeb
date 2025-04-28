fe9f90d0758b225e9a1a21ad217ff15210a15b5f
Ailafeka <ailafeka@gmail.com>
2025-04-28 14:10:47 +0800

    修正vueLazyLoad路徑

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import VueLazyload from 'vue-lazyload'
 const app = createApp(App);
 app.use(VueLazyload,{
-    loading:"@/assets/preview3.jpg",
+    loading:"/assets/preview3.jpg",
     error:''
 })
 
